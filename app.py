31a9a7442ed55f70edba1e7a2416d8e0368d7678
d-mbithi <denmbithi@gmail.com>
2018-12-23 17:20:34 +0300

    added authentication system

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template, redirect, url_for, request
-app = Flask(__name__)
+from flask import (
+        Flask, render_template, redirect, url_for, request, session, flash
+        )
+from functools import wraps
 
+
+# create the application and set some configurations
+app = Flask(__name__)
+app.secret_key = 'klasndlnqenio32ea/dmmkawd2nqeks[[qeslkwqelk'
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to log in')
+            return redirect(url_for('login'))
+    return wrap
 
 # funtion route for home page
 @app.route('/')
+@login_required
 def home():
     return render_template('index.html')
 
@@ -14,7 +33,7 @@ def welcome():
     return render_template("welcome.html")
 
 
-# Function page for login page
+# login page routing
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -23,8 +42,19 @@ def login():
                 request.form['password'] != 'admin':
                     error = 'Invalid login cridentials, please try again'
         else:
+            session['logged_in'] = True
+            flash('you have been loggen in!')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+
+# Logout page routing
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('you have been loggen out!')
+    return redirect(url_for('welcome'))
 
 
 if __name__ == "__manin__":
